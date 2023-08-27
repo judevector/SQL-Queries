3ea76274c101df2e8be46e2ee3cce3290ab4f5f4
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-08-27 22:40:35 +0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -9,3 +9,11 @@ SELECT * FROM products WHERE is_sale != false;
 SELECT * FROM products WHERE inventory != 0 AND price > 20;
 SELECT * FROM products WHERE name LIKE 'TV%';
 SELECT * FROM products WHERE name NOT LIKE '%en%';
+SELECT * FROM products WHERE price > 50 ORDER by id DESC;
+SELECT * FROM products WHERE price > 50 ORDER by created_at DESC;
+SELECT * FROM products ORDER by id LIMIT 5;
+SELECT * FROM products ORDER by id LIMIT 5 OFFSET 2;
+INSERT INTO products (name, price, inventory) VALUES ('piano', 160, 150);
+INSERT INTO products (name, price, inventory) VALUES ('piano', 160, 150) returning *;
+DELETE FROM products WHERE name = 'piano' ;
+UPDATE products SET name = 'MyCar', price = 100 WHERE id = 5 RETURNING *
