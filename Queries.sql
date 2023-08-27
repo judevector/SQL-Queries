0ad7e15d4d0b3a7152d2b7504cb7c3cc32dd298c
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-08-27 17:41:26 +0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,5 @@
 SELECT * FROM products;
 SELECT id FROM products;
 SELECT id AS product_id, is_sale AS on_sale FROM products;
-SELECT * FROM products WHERE id = 10
+SELECT * FROM products WHERE id = 10;
+SELECT id, name FROM products WHERE id = 10;
